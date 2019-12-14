cca5506568de60c225e1a68269b4d8f18b055421
U-DESKTOP-6S5BTB7\Kitti <k.kaigitti@gmail.com>
2019-12-14 13:25:51 +0700

    close #42 - ทำการแก้ไข UI ของระบบจัดการเมนูอาหาร

--- frontend/src/components/MenuManager.vue
+++ frontend/src/components/MenuManager.vue
@@ -1,65 +1,63 @@
 <template>
   <div class="mainpage">
-    <v-toolbar>
-      <v-toolbar-title>Menu Manager - Restaurant</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>User : Kaigitti</v-toolbar-title>
-      <v-btn depressed small>Logout</v-btn>
-    </v-toolbar>
 
-    <div class="outside">
-      <br>
-      <v-container>
-        <v-col cols = 3>
-          <v-text-field
-          v-model="firstname"
-          :rules="nameRules"
-          :counter="20"
-          label="Menu Name"
-          required
-          ></v-text-field>
-          <v-text-field
-          v-model="firstname"
-          :rules="nameRules"
-          :counter="20"
-          label="Menu Price"
-          required
-          ></v-text-field>
-          <br>
-          <v-combobox
-          v-model="select"
-          :items="items"
-          label="Menu Type"
-          multiple
-          outlined
-          dense
-          ></v-combobox>
-          <br>
-          <v-combobox
-          v-model="select"
-          :items="items"
-          label="Menu Ingredient"
-          multiple
-          outlined
-          dense
-          ></v-combobox>
-          <br>
-          <v-combobox
-          v-model="select"
-          :items="items"
-          label="Menu Category "
-          multiple
-          outlined
-          dense
-          ></v-combobox>
-          <br>
-          <v-col>
-            <v-btn small >Add Menu</v-btn>
+    <br>
+    <v-container>
+      <v-col cols = 4>
+        <v-card>
+          <v-col cols = 12 align="center">
+            <h1>Menu Manager</h1>
+            <br>
+            <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="20"
+            label="Menu Name"
+            required
+            ></v-text-field>
+            <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="20"
+            label="Menu Price"
+            required
+            ></v-text-field>
+            <br>
+            <v-combobox
+            v-model="select"
+            :items="items"
+            label="Menu Type"
+            multiple
+            outlined
+            dense
+            ></v-combobox>
+            <br>
+            <v-combobox
+            v-model="select"
+            :items="items"
+            label="Menu Ingredient"
+            multiple
+            outlined
+            dense
+            ></v-combobox>
+            <br>
+            <v-combobox
+            v-model="select"
+            :items="items"
+            label="Menu Category "
+            multiple
+            outlined
+            dense
+            ></v-combobox>
+            <br>
+            <v-col>
+              <v-btn small >Add Menu</v-btn>
+            </v-col>
           </v-col>
-        </v-col>
+        </v-card>
+      </v-col>
 
-      </v-container>
-    </div>
+    </v-container>
 
   </div>
 </template>
