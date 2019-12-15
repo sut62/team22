feeaaa8125309b98ac66c7d3953501e0f2bec0b6
wasanmsn <wasangg@gmail.com>
2019-12-15 19:16:32 +0700

    forgot to add Controller files T_T #17

---
+++ backend/src/main/java/com/okta/springbootvue/controller/ReservationsController.java
@@ -0,0 +1,55 @@
+package main.java.com.okta.springbootvue.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+
+import com.okta.springbootvue.entity.*;
+import com.okta.springbootvue.repository.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class ReservationsController {
+
+    @Autowired
+    private final ReservationsRepository ReservationsRepository;
+
+    public ReservationsController(ReservationsRepository ReservationsRepository) {
+        this.ReservationsRepository = ReservationsRepository;
+    }
+
+    @GetMapping("/reservs")
+    public Collection<newresavations> newresavations() {
+        return ReservationsRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @PostMapping("/reservs/{member_id}/{table_id}/{service_id}/{reservs_date}/{reservs_time}/{seats}")
+    public reservations newresavations(reservations newresavations,
+  @PathVariable long member_id,
+  @PathVariable long table_id,
+  @PathVariable long service_id,
+  @PathVariable String reservs_date,
+  @PathVariable String reservs_time,
+  @PathVariable String seats
+  
+  ) {
+  
+    Member reservefor = MemberRepository.findById(member_id);
+    tables has = TableRepository.findById(table_id);
+    services serviceto = ServicesRepository.findById(service_id);
+    LocalDateTime datetime = LocalDateTime.parse(reservs_date+"T"+reservs_time);
+    
+    newresavations.setReservefor(reservefor);
+    newresavations.setHas(has);
+    newresavations.setServiceto(serviceto);
+    newresavations.setSeats(seats);
+    newresavations.setReservedateandtime(datetime);
+    return ReservationsRepository.save(newresavations); 
+  }
+}
