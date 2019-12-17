374817adce294c95890fbae22c32d5fbd065d12e
apower2499 <apower2499@gmail.com>
2019-12-17 19:15:42 +0700

    close #52 -ทำRepositoryทั้งหมดของEmployee

--- backend/src/main/java/com/okta/springbootvue/repository/EmployeeRepository.java
+++ backend/src/main/java/com/okta/springbootvue/repository/EmployeeRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public
 interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Employee findById(long id);
+	Employee findById(long id);
 }
