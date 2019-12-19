c0afa1b2ceb044d15863064a33303e7024d949e8
apower2499 <apower2499@gmail.com>
2019-12-19 19:58:47 +0700

    close #30 ทำcontrollerของระบบลงทะเบียนพนักงาน

---
+++ backend/src/main/java/com/okta/springbootvue/controller/GenderController.java
@@ -0,0 +1,32 @@
+package com.cpe.backend.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.cpe.backend.entity.Gender;
+import com.cpe.backend.repository.GenderRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class GenderController {
+
+    @Autowired
+    private final GenderRepository genderRepository;
+
+    public GenderController(GenderRepository genderRepository) {
+        this.genderRepository = genderRepository;
+    }
+
+    @GetMapping("/Gender")
+    public Collection<Gender> Genders() {
+        return genderRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
