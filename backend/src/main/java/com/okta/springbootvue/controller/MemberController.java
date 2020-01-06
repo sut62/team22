c502cc9c66b6e7c5c0020830b2f6eeb6c5999a0b
moosorasich <57953687+moosorasich@users.noreply.github.com>
2020-01-06 18:05:16 +0700

    Update MemberController.java
    เพิ่ม import post,path

--- backend/src/main/java/com/okta/springbootvue/controller/MemberController.java
+++ backend/src/main/java/com/okta/springbootvue/controller/MemberController.java
@@ -25,6 +25,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
