d5b17a8262473d6a25f50fcd3196600edb6b0957
moosorasich <57953687+moosorasich@users.noreply.github.com>
2020-01-06 18:31:15 +0700

    Update MemTypeRepository.java

--- backend/src/main/java/com/okta/springbootvue/repository/MemTypeRepository.java
+++ backend/src/main/java/com/okta/springbootvue/repository/MemTypeRepository.java
@@ -1,12 +1,12 @@
 package com.okta.springbootvue.repository;
 
-import com.okta.springbootvue.entity.MenuType;
+import com.okta.springbootvue.entity.MemType;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public
-interface MemTypeRepository extends JpaRepository<MenuType, Long> {
-    MenuType findById(long id);
+interface MemTypeRepository extends JpaRepository<MemType, Long> {
+    MemType findById(long id);
 }
