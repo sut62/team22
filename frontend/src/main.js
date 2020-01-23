1c80635b46695a32dca6e9a61379c4e0c64bedf9
Supkaorn_oh <ohoh2425@hotmail.com>
2020-01-24 00:53:55 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-<<<<<<< HEAD
 import firebase from 'firebase/app';
 import nav from '@/components/sidebar.vue'
 import appbar from '@/components/appbar.vue'
@@ -23,14 +22,3 @@ firebase.auth().onAuthStateChanged(() => {
       }).$mount('#app')
   }
 });
-
-=======
-import VueSimpleAlert from "vue-simple-alert";
-Vue.config.productionTip = false
-Vue.use(VueSimpleAlert);
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
->>>>>>> issue-114
