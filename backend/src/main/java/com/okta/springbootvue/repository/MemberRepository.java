11fc5f9dfccff8927d21878b06d4c400a9ad8184
moosorasich <moosorasich@hotmail.com>
2020-01-23 20:39:17 +0700

    close #94 ทำfunctionค้นหา

--- backend/src/main/java/com/okta/springbootvue/repository/MemberRepository.java
+++ backend/src/main/java/com/okta/springbootvue/repository/MemberRepository.java
@@ -1,12 +1,19 @@
 package com.okta.springbootvue.repository;
 
+import java.util.Optional;
+
 import com.okta.springbootvue.entity.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public
 interface MemberRepository extends JpaRepository<Member, Long> {
     Member findById(long id);
+
+    @Query(value = "SELECT * FROM MEMBER WHERE TEL = :tel",nativeQuery = true)
+    Optional<Member> findByTel(@Param("tel") String tel);
 }
