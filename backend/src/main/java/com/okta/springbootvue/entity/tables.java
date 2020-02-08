1510f2b758c54bd50cb80c0be9eaed6cb6b9819d
wasanmsn <wasangg@gmail.com>
2020-02-08 19:58:37 +0700

    cleaning

--- backend/src/main/java/com/okta/springbootvue/entity/tables.java
+++ backend/src/main/java/com/okta/springbootvue/entity/tables.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
-import javax.validation.constraints.Pattern;
+
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.NotNull;
 
